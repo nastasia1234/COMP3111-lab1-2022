cf76234f1e642b18d5968b82b52d2276e942d48a
Nastasia Morel <nastasia.morel@epfl.ch>
2022-09-18 22:04:23 +0800

    I have made a change

--- java/Library.java
+++ java/Library.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Library {
+public class Library {
+
+    /* Add this function */
+    public static void main(String arg[]) {
+        Library lib = new Library();
+        if (lib.someLibraryMethod())
+            System.out.println("Hello, World!");
+    }
+
+
+    public boolean someLibraryMethod() {
+        return true;
+    }
 }
